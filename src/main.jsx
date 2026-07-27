bc46f2e3b5ec1f0c5be77e37440a6a71b2879aac
272Dev <zks.sigma@gmail.com>
2026-07-27 01:40:21 -0300

    Activate generator permission manager

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './AppNexus.jsx';
+import './stylesNexus.css';
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
 
